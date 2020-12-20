a166dcf3c58caaad5fb21fabd5e79f7d09641029
Caleb Eby <caleb.eby01@gmail.com>
2020-12-20 15:56:26 -0800

    Update / remove deps

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 
 /** @type {import('rollup').RollupOptions} */
 const options = {
-  input: { rollup: './src/rollup', preprocessor: './src/preprocessor' },
+  input: { rollup: './src/rollup' },
   plugins: [
     babel({ babelHelpers: 'bundled', extensions: ['.js', '.ts'] }),
     nodeResolve({ extensions: ['.js', '.mjs', '.ts'] }),
@@ -11,16 +11,6 @@ const options = {
   output: [
     { dir: 'dist', format: 'cjs', entryFileNames: '[name].cjs' },
     { dir: 'dist', format: 'esm', entryFileNames: '[name].mjs' },
-  ],
-  external: [
-    'astring',
-    'vm',
-    'magic-string',
-    'path',
-    'es-module-lexer',
-    'fs',
-    'util',
-    'crypto',
   ],
 }
 
